d1890b20c518d12bc0f83d308683b1f17ac1df0e
Blessin George Varghese <46672514+bgrgv@users.noreply.github.com>
2019-04-20 01:15:11 +0530

    Update app.py
    Added template.

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
-    return 'DSC SRM Certificates Portal TO_BE!'
-
+    return render_template('base.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
